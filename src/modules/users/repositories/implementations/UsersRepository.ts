b491268ec0568795c0657120deff051e8bc5656b
Paulo Vieira <paullocostta86@gmail.com>
2021-03-20 10:39:05 -0300

    refactor: code UserRepository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,44 +14,30 @@ class UsersRepository implements IUsersRepository {
     if (!UsersRepository.INSTANCE) {
       UsersRepository.INSTANCE = new UsersRepository();
     }
-
     return UsersRepository.INSTANCE;
   }
 
   create({ name, email }: ICreateUserDTO): User {
     const user = new User();
-
-    Object.assign(user, {
-      name,
-      email,
-    });
-
+    Object.assign(user, { name, email });
     this.users.push(user);
-
     return user;
   }
 
   findById(id: string): User | undefined {
     const user = this.users.find((user) => user.id === id);
-
     return user;
   }
 
   findByEmail(email: string): User | undefined {
     const user = this.users.find((user) => user.email === email);
-
     return user;
   }
 
   turnAdmin(receivedUser: User): User {
     const index = this.users.findIndex((user) => user === receivedUser);
-
-    Object.assign(receivedUser, {
-      admin: !receivedUser.admin,
-    });
-
+    Object.assign(receivedUser, { admin: !receivedUser.admin });
     this.users[index] = receivedUser;
-
     return receivedUser;
   }
 
